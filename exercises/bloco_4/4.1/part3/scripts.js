e987058f100d9b07e07a10d5c2cf9efa101e5255
Emídio Rebouças <emidioreb@gmail.com>
2021-05-19 18:47:30 -0300

    resolução 4.1 part3

---
+++ exercises/bloco_4/4.1/part3/scripts.js
@@ -0,0 +1,20 @@
+//let patientId = "50";
+//let isEnrolled = true;
+//const patientInfo = {
+  //firstName: 'Ana',
+  //lastName: 'Santos',
+//};
+//const patientEmail = 'ana@email.com';
+
+//console.log(typeof patientId) 
+//console.log(typeof patientAge)
+
+// Vamos fazer algumas operações simples para encontrarmos a área e o perímetro de um retângulo de base 5 e altura 8.
+
+let base = 5;
+let altura = 8;
+let area = (base * altura)
+let perimetro = (2*base)+(2*altura)
+
+console.log(area)
+console.log(perimetro)
