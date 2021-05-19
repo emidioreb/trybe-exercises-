522cde8d5848dc5d287e82eaa0fd75a44c445e36
Emídio Rebouças <emidioreb@gmail.com>
2021-05-19 16:42:58 -0300

    execução 4.1 parte 2

---
+++ exercises/bloco_4/4.1/part2/scripts.js
@@ -0,0 +1,8 @@
+const name = "emidio";
+const birthCity = "Cedro";
+let birthYear = 1996;
+birthYear = 2030;
+birthCity = "Fortaleza"
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);
