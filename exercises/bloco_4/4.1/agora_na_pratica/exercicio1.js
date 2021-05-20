07bbe362b5fd25628f02cb84ad07bf69bc1752fa
Emídio Rebouças <emidioreb@gmail.com>
2021-05-20 10:34:03 -0300

    finalizado parte 5 e exercício 1

---
+++ exercises/bloco_4/4.1/agora_na_pratica/exercicio1.js
@@ -0,0 +1,13 @@
+let a = 20;
+let b = 20;
+let soma = a+b;
+let subtração = a-b;
+let multiplicação = (a*b);
+let divisão = (a/b);
+let módulo = (a%b);
+
+console.log("Soma = ",soma);
+console.log("Subtração = ",subtração);
+console.log("multiplicação = ",multiplicação);
+console.log("divisão = ",divisão);
+console.log("Módulo = ",módulo);
