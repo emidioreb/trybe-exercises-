01c9692de1446da734770f7525fca3c62385e0bc
Emídio Rebouças <emidioreb@gmail.com>
2021-05-31 00:18:52 -0300

    realizacao do exercicio 3 ao 8

--- exercises/script.js
+++ exercises/script.js
@@ -48,12 +48,103 @@ createDaysMonth();
 function createButton(newbutton){
   const buttonContainer = document.querySelector('.buttons-container');
   const buttonSon = document.createElement('button');
-  const buttonSonid = 'btn-holiday';
-  buttonSon.id = buttonSonid; 
+  buttonSon.id = 'btn-holiday';
   buttonContainer.appendChild(buttonSon);
   buttonSon.innerHTML = newbutton;
-
 };
 
 createButton('feriado');
+
+function displayHolidays() {
+  let getHolidayButton = document.querySelector('#btn-holiday');
+  let getHolidays = document.querySelectorAll('.holiday')
+  let backgroundColor = 'rgb(238,238,238)';
+  let setNewColor = 'white';
+
+  getHolidayButton.addEventListener('click', function() {
+    for (let index = 0; index < getHolidays.length; index += 1) {
+      if (getHolidays[index].style.backgroundColor === setNewColor) {
+        getHolidays[index].style.backgroundColor = backgroundColor;
+      } else {
+        getHolidays[index].style.backgroundColor = setNewColor;
+      }
+    }
+  })
+};
+
+displayHolidays();
+
+function createButtonFriday (newButtonFriday){
+  const buttonContainer = document.querySelector(".buttons-container"); 
+  const buttonFriday = document.createElement('button');
+  buttonFriday.id = 'btn-friday'
+  buttonContainer.appendChild(buttonFriday)
+  buttonFriday.innerHTML = newButtonFriday;
+}
+
+createButtonFriday('Sexta-feira')
+
+function displayFridays(fridaysArray) {
+  let getFridayButton = document.querySelector('#btn-friday');
+  let fridays = document.getElementsByClassName('day friday');
+  let newFridayText = 'SEXTOU o/';
+
+  getFridayButton.addEventListener('click', function() {
+  for (let index = 0; index < fridays.length; index += 1) {
+    if (fridays[index].innerHTML !== newFridayText) {
+        fridays[index].innerHTML = newFridayText;
+    } else {
+        fridays[index].innerHTML = fridaysArray[index];
+      }
+    }
+  })
+};
+
+let dezFridays = [ 4, 11, 18, 25 ];
+
+function dayMouseOver() {
+  let days = document.querySelector('#days');
+
+  days.addEventListener('mouseover', function(event) {
+    event.target.style.fontSize = '30px';
+    event.target.style.fontWeight = '600';
+  })
+}; 
+function dayMouseOut() {
+  let days = document.querySelector('#days');
+
+  days.addEventListener('mouseout', function(event) {
+    event.target.style.fontWeight = '200';
+    event.target.style.fontSize = '20px';
+  })
+};
+ 
+
+dayMouseOver();
+dayMouseOut();
+
+function addTask (parament){
+  const mytask = document.querySelector('.my-tasks');
+  const getAddTask = document.createElement('span');
+  getAddTask.id = 'add-task';
+  mytask.appendChild(getAddTask);
+
+  getAddTask.innerHTML = parament;
+}
+
+addTask('cozinhar');
+
+function addColorTask (color){
+  const addTask = document.querySelector('#add-task');
+  const mytask = document.querySelector('.my-tasks');
+  const getAddColorTask = document.createElement('div');
+  getAddColorTask.className = 'task';
+  mytask.appendChild(getAddColorTask);
+
+  getAddColorTask.style.backgroundColor = color;
+  
+}
+
+addColorTask('green');
+
 
