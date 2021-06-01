6cdfd6e23e1a0a52e19c0035c3ea43c22225b070
Emídio Rebouças <emidioreb@gmail.com>
2021-05-31 23:40:11 -0300

    realizacao da questao 8 e 9

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/script.js
@@ -52,12 +52,30 @@
 //}
 
 // exercicio 7
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let menor = numbers[0];
-for (let index = 0; index < numbers.length; index +=1) {
-  if(numbers[index] < menor){
-    menor = numbers[index];
-  }   
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let menor = numbers[0];
+//for (let index = 0; index < numbers.length; index +=1) {
+//  if(numbers[index] < menor){
+//    menor = numbers[index];
+//  }   
+//}
+//console.log(menor); 
+
+exercicio 8
+let numbers = [];
+
+for (let index = 1; index <= 25; index += 1) {
+  numbers.push(index);
 }
-console.log(menor); 
- 
+
+console.log(numbers);
+
+//exercicio 9
+
+array = [1, 2, 3, 4, 5, 6, 7, 8,9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+for (let index = 0; index < array.length; index += 1) {
+  result = array[index]/2
+  console.log(result);  
+}
+
+
