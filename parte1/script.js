2d72f099f959399a0329a8ccd2a64c76986e88d7
Emídio Rebouças <emidioreb@gmail.com>
2021-06-21 23:12:57 -0300

    atualizando

---
+++ parte1/script.js
@@ -0,0 +1,20 @@
+// //questão 1
+// const assert = require('assert');
+
+// function sum(a, b) {
+//   if (typeof a !== 'number' || typeof b !== 'number') {
+//     throw new Error('parameters must be numbers');
+//   }
+
+//   return a + b;
+// }
+// //Teste se o retorno de sum(4, 5) é 9
+// assert.strictEqual(sum(4,5),9,'quatro mais cinco é igual nove')
+// //Teste se o retorno de sum(0, 0) é 0
+// assert.strictEqual(sum(0,0),0,'zero mais zero é igual zero')
+// //Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
+// assert.strictEqual(sum(4,'5'),0,'zero mais zero é igual zero')
+
+// -------------------------------------------------------------------------------------------------
+
+//questão 2
