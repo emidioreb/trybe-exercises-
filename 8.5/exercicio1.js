174f1c148eeb6bba5d1c8af40b3649e020bfc0d1
Emídio Rebouças <emidioreb@gmail.com>
2021-07-06 15:26:47 -0300

    exercicios do 8.5

--- 8.5/exercicio1.js
+++ 8.5/exercicio1.js
@@ -1,7 +1,9 @@
-function quantosParams(...args) {
-  console.log('parâmetros:', args);
-  return `Você passou ${args.length} parâmetros para a função.`;
-}
+const product = {
+  name: 'Smart TV Crystal UHD',
+  price: '1899.05',
+  seller: 'Casas de Minas',
+};
 
-console.log(quantosParams(0, 1, 2)); // Você passou 3 parâmetros para a função.
-console.log(quantosParams('string', null, [1, 2, 3], { })); // Você passou 4 parâmetros para a função.
+const { name, seller } = product;
+console.log(name); // Smart TV Crystal UHD
+console.log(seller); 
