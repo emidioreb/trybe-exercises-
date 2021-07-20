5c5de2808a95102891c75cdde065a88d2e4bb27a
Emídio Rebouças <emidioreb@gmail.com>
2021-07-20 19:02:40 -0300

    iniciando bloco 11

---
+++ reb-app/src/App.js
@@ -0,0 +1,21 @@
+    
+    import React from 'react';
+    import './App.css';
+
+    const Task = (value) => { // Função feita para entrar dentro do componente App
+      return ( // colocar esse return é obrigatório
+        <li key={value}>{value}</li> // Li acompanhando da notação correta para ser inseridos valores
+      );
+    }
+    // Criação do array com as atividades diárias
+    const tarefas = ['Acordar', 'Tomar café', 'Escovar os dentes', 'Ir trabalhar'];
+
+    class App extends React.Component { // Criação do componente APP
+      render() { // colocar esse render é obrigatório
+        return ( // colocar esse return é obrigatório
+          <ul>{ tarefas.map(tarefa => Task(tarefa)) }</ul> // Ul é uma lista desordenada e os li ficam dentro dela
+        ); // O map vai buscar cada atividade e adicionar na lista
+      }
+    }
+
+    export default App;
